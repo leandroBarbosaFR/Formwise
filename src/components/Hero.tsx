ac3a0e6b717f0c69cbcdd08f1e8c65c7d075eb00
Leandro Barbosa <leandrobarbosa@sfr.fr>
2025-05-05 18:12:17 +0200

    - clean up and add new icon and new favicons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,25 +32,11 @@ export function Hero() {
         Avec notre solution, gérez facilement les inscriptions de manière
         simple, rapide et efficace.
       </p>
-      <div className="mt-10 flex justify-center gap-x-6">
+      <div className="mt-10 flex flex-col justify-center gap-x-6 gap-y-5 md:flex-row">
         <Button
           style={{ gap: '10px' }}
           href="/https://calendly.com/hello1367studio/30min"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-phone-icon lucide-phone"
-          >
-            <path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384" />
-          </svg>
           <span>Réserver un appel</span>
         </Button>
         <Button href="/contact" variant="outline" style={{ gap: '10px' }}>
